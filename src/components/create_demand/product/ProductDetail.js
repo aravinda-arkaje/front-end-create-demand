d39d769730ba3fd16374c1035f7c575f96e341d4
Aravinda <aravindab@datatemplate.com>
2023-04-04 12:58:43 +0530

    Product Details

--- src/components/create_demand/product/ProductDetail.js
+++ src/components/create_demand/product/ProductDetail.js
@@ -64,6 +64,7 @@ export default function ProductDetail(props) {
 
     return (
         <div>
+            {/* Product Details */}
             {props.selectedProduct ?
             <Product>
                 <ProductDetailHead>Product Details</ProductDetailHead>
